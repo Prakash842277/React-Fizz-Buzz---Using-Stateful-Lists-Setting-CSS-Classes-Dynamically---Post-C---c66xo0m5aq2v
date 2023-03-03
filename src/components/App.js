d143e3ca0572699b396528d98f95c8dd82b48710
Prakash842277 <121437397+Prakash842277@users.noreply.github.com>
2023-03-03 13:22:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,62 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+// import React from 'react'
+// import '../styles/App.css';
+// const App = () => {
 
   
+//   return (
+//     <div id="main">
+
+//     </div>
+//   )
+// }
+
+
+// export default App;
+
+
+
+
+
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
+const App = () => {
+  const [count, setCount] = useState(1);
+  const [className, setClassName] = useState("normal");
+  useEffect(() => {
+    if (count % 15 === 0) {
+      setClassName("fizzbuzz");
+    } else if (count % 5 === 0) {
+      setClassName("buzz");
+    } else if (count % 3 === 0) {
+      setClassName("fizz");
+    } else {
+      setClassName("normal");
+    }
+  }, [count]);
+
   return (
     <div id="main">
-
+      <button
+        id="increment"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <div id="counter" className={className}>
+        {count}
+      </div>
+      <button
+        id="decrement"
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Increment
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
